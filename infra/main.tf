7e82a9188de41bea50088fabdd82b2fb40eb6d01
Juan Diego <silgajuandiego@gmail.com>
2018-03-19 10:41:36 -0500

    [Add] terraform files

---
+++ infra/main.tf
@@ -0,0 +1,48 @@
+resource "digitalocean_loadbalancer" "curso-devops" {
+  name   = "platzi-html-v2"
+  region = "nyc3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+  }
+
+  droplet_tag = "${digitalocean_tag.curso-devops.name}"
+}
+
+resource "digitalocean_tag" "curso-devops" {
+  name = "platzi-html"
+}
+
+resource "digitalocean_droplet" "curso-devops" {
+  count    = 3
+  image    = "32629659"
+  name     = "devops-curso-v2"
+  region   = "nyc3"
+  size     = "512mb"
+  ssh_keys = [19191407]
+  tags     = ["${digitalocean_tag.curso-devops.id}"]
+
+  user_data = <<EOF
+#cloud-config
+coreos:
+  units:
+    - name: "platzi.service"
+      command: "start"
+      content: |
+        [Unit]
+        Description=Platzi Demo
+        After=docker.service
+
+        [Service]
+        ExecStart=/usr/bin/docker run -d -p 3000:3000 devops-curso
+EOF
+}
