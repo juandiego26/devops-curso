45c2172e981db5b8be463fd0cf544347562c4fbe
Juan Diego <silgajuandiego@gmail.com>
2018-03-20 08:53:01 -0500

    [Add] Full Deployment Pipeline

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,9 +1,10 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.46879015.1437742939.1521475479-650164964.1521159473
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.4/terraform_0.11.4_linux_amd64.zip?_ga=2.166740030.1883617524.1521468662-1195488747.1521202943
+packer version
 unzip /tmp/packer.zip -d ~/bin
 unzip /tmp/terraform.zip -d ~/bin
-~/bin/packer validate deployments/template.json &&
-~/bin/packer build deployments/template.json &&
+packer validate deployments/template.json &&
+packer build deployments/template.json &&
 export TF_VAR_image_id=$(curl -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" https://api.digitalocean.com/v2/images?private=true | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id")
 echo $TF_VAR_image_id
 cd infra terraform init -input=false && terraform apply -input=false -auto-approve && cd .. &&
