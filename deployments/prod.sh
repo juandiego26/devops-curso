6f392766996bdc4a4ba0decbcc00c247e829a104
Juan Diego <silgajuandiego@gmail.com>
2018-03-20 13:55:12 -0500

    [Add] Full Deployment Pipeline

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -6,6 +6,6 @@ unzip /tmp/terraform.zip -d ~/bin
 ~/bin/packer build deployments/template.json &&
 export TF_VAR_image_id=$(curl -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" https://api.digitalocean.com/v2/images?private=true | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id")
 echo $TF_VAR_image_id
-cd infra && ~/bin/terraform && cd .. &&
+cd infra && ~/bin/terraform apply && cd .. &&
 git add infra && git commit -m 'Deployed $CIRCLE_BUILD_NUM [skip ci]' &&
 git push origin master
